776afce81abcb83d29ac45d6083a8b8453c81e64
Benjamin Lakis <benlakis@student.purdueglobal.edu>
2024-11-20 06:17:45 +0000

    Added Wildcard loader, selector and str display

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from .on_the_fly_wildcards import WildcardCreator
+from .on_the_fly_wildcards import WildcardCreator, WildcardLoader, WildcardSelector, DisplayString
 
 NODE_CLASS_MAPPINGS = {
-    "Wildcard Creator": WildcardCreator
+    "Wildcard Creator": WildcardCreator,
+    "Wildcard Loader": WildcardLoader,
+    "Wildcard Selector": WildcardSelector,
+    "Display String": DisplayString
 }
--- on_the_fly_wildcards.py
+++ on_the_fly_wildcards.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class WildcardCreator:
     def __init__(self, **kwargs):
@@ -21,17 +22,80 @@ class WildcardCreator:
     CATEGORY = "Wildcards"
 
     def create_wildcard(self, wildcard_text, filename_prefix, save_to_file):
+        # Define Paths
+        base_path = "ComfyUI/wildcards"
+        additional_paths = [
+            "ComfyUI/custom_nodes/ComfyUI-Impact-Pack/wildcards",
+            "ComfyUI/custom_nodes/ComfyUI-Impact-Pack/custom_wildcards",            
+        ]
+
+        # Ensure base path exists
+        os.makedirs(base_path, exist_ok=True)
+
         # Genereate a unique filename
         i = 1
         filename = f"{filename_prefix}{i}.txt"
-        while os.path.exists(os.path.join("wildcards", filename)):
+        while os.path.exists(os.path.join(base_path, filename)):
             i += 1
             filename = f"{filename_prefix}{i}.txt"
 
         # Save the wildcard text to file if requested
         if save_to_file:
-            with open(os.path.join("wildcards", filename), "w") as f:
+            with open(os.path.join(base_path, filename), "w") as f:
                 f.write(wildcard_text)
 
         # Return the filename for referencing in prompts
         return (filename,)
+
+class WildcardLoader:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "directory": ("STRING", {"default": "ComfyUI/wildcards"}),
+            },
+        }
+    
+    RETURN_TYPES = ("LIST",)
+    FUNCTION = "load_wildcards"
+    CATEGORY = "Wildcards"
+
+    def load_wildcards(self, directory):
+        # Load all wildcard files from the specified directory
+        files = os.listdir(directory)
+        wildcards = [f for f in files if (f.endswith('.txt') | f.endswith('.yaml'))]
+        return (wildcards,)
+    
+class WildcardSelector:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "wildcard_list": ("LIST", {}),
+            },
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "select_wildcards"
+    CATEGORY = "Wildcards"
+
+    def select_wildcard(self, wildcard_list):
+        # Randomly select a wildcard from the list
+        return (random.choice(wildcard_list),)
+
+class DisplayString:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "input_string": ("STRING", {}),
+            },
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "display_string"
+    CATEGORY = "Display"
+
+    def display_string(self, input_string):
+        # Simply return the input string for display
+        return (input_string,)
